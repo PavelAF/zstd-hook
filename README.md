7af5deefb7809dd923498bdb3e997b0140d0310a
Like Ma <likemartinma@gmail.com>
2022-10-08 22:11:47 +0800

    Hook zstd for compression level and threads
    Hook `ZSTD_CCtx* ZSTD_createCCtx(void)` to set compression level
    and compression threads by the following environment variables:
    
     * `ZSTD_CLEVEL` : the zstd compression level
     * `ZSTD_NBTHREADS` : the zstd compression threads

---
+++ README.md
@@ -0,0 +1,33 @@
+# Hook zstd for setting compression level and compression threads
+
+A shared library to hook `ZSTD_CCtx* ZSTD_createCCtx(void)` for setting compression level and compression threads.
+
+For `qemu-img` (>= **6.0**) or the similar programs without setting zstd compression level and compression threads options.
+
+See also: [How to change zstd level for qcow2 image?](https://stackoverflow.com/questions/72562226/how-to-change-zstd-level-for-qcow2-image)
+
+## Building
+
+```shell
+cd zstd-hook
+cmake -DCMAKE_BUILD_TYPE=Release
+make
+```
+
+## Usage
+
+Set the following environment variables:
+
+* `ZSTD_CLEVEL` : the zstd compression level between `1` and `22`
+* `ZSTD_NBTHREADS` : the zstd compression threads
+* `LD_PRELOAD` : the hook library `libzstdhook.so`
+* `LD_LIBRARY_PATH` : the directory for the hook library `libzstdhook.so`
+
+For examples, hook `qemu-img` (>= **6.0**) converting source image to zstd compressed `qcow2` image by compression level `22` and the **maximum** compression threads on Ubuntu 22.04:
+
+```shell
+cd zstd-hook
+LD_LIBRARY_PATH=. LD_PRELOAD=libzstdhook.so \
+  ZSTD_CLEVEL=22 ZSTD_NBTHREADS=`grep -c '^processor' /proc/cpuinfo` \
+  qemu-img convert -c -o compression_type=zstd -O qcow2 <origin-image> <compressed-qcow2>
+```
---
+++ zstd_hook.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <sys/param.h>
+
+#define DLL_Export __attribute__((visibility("default")))
+
+typedef enum {
+    ZSTD_c_compressionLevel = 100,
+    ZSTD_c_nbWorkers = 400,
+} ZSTD_cParameter;
+
+enum {
+    ZSTD_MAX_CLEVEL = 22,
+};
+
+typedef struct ZSTD_CCtx_s ZSTD_CCtx;
+typedef ZSTD_CCtx* (*ZSTD_createCCtx_t)(void);
+typedef size_t (*ZSTD_CCtx_setParameter_t)(ZSTD_CCtx* cctx, ZSTD_cParameter param, int value);
+typedef int (*ZSTD_maxCLevel_t)(void);
+
+static void* zstd_handle;
+static ZSTD_createCCtx_t orig_ZSTD_createCCtx;
+static ZSTD_CCtx_setParameter_t orig_ZSTD_CCtx_setParameter;
+static ZSTD_maxCLevel_t orig_ZSTD_maxCLevel;
+
+void __attribute__((destructor)) fini_zstd_symbols()
+{
+    if (zstd_handle) {
+        orig_ZSTD_createCCtx = NULL;
+        orig_ZSTD_CCtx_setParameter = NULL;
+        orig_ZSTD_maxCLevel = NULL;
+        dlclose(zstd_handle);
+    }
+}
+
+void __attribute__((constructor)) init_zstd_symbols()
+{
+    if (zstd_handle)
+        return;
+
+    if (!(zstd_handle = dlopen("libzstd.so.1", RTLD_LAZY))) {
+        fini_zstd_symbols();
+        return;
+    }
+
+    orig_ZSTD_createCCtx = (ZSTD_createCCtx_t) dlsym(
+            zstd_handle, "ZSTD_createCCtx");
+
+    orig_ZSTD_CCtx_setParameter = (ZSTD_CCtx_setParameter_t) dlsym(
+            zstd_handle, "ZSTD_CCtx_setParameter");
+
+    orig_ZSTD_maxCLevel = (ZSTD_maxCLevel_t) dlsym(
+            zstd_handle, "ZSTD_maxCLevel");
+}
+
+DLL_Export ZSTD_CCtx* ZSTD_createCCtx(void)
+{
+    if (!orig_ZSTD_createCCtx)
+        return NULL;
+
+    ZSTD_CCtx* const cctx = orig_ZSTD_createCCtx();
+    if (!cctx || !orig_ZSTD_CCtx_setParameter)
+        return cctx;
+
+    const char* v = getenv("ZSTD_CLEVEL");
+    if (v && *v) {
+        const int max_level = orig_ZSTD_maxCLevel ?
+            orig_ZSTD_maxCLevel() : ZSTD_MAX_CLEVEL;
+
+        const int level = MIN(atoi(v), max_level);
+        if (level > 0)
+            orig_ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, level);
+    }
+
+    v = getenv("ZSTD_NBTHREADS");
+    if (v && *v) {
+        const int workers = atoi(v);
+        if (workers > 0)
+            orig_ZSTD_CCtx_setParameter(cctx, ZSTD_c_nbWorkers, workers);
+    }
+
+    return cctx;
+}
+// vim: set et ts=4 sts=4 sw=4:
